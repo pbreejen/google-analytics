599f14fb55c3104b470c319a9307fdf7f169d346
Paul den Breejen <paul.denbreejen@springer.com>
2019-03-03 12:38:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='googleanalytics',
     ],
     include_package_data=True,
     install_requires=[
-        'oauth2client==1.5.2',
+        'oauth2client==2.0',
         'google-api-python-client==1.5.0',
         'python-dateutil',
         'addressable>=1.4.2',
